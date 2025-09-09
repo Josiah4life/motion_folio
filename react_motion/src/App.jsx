7be769b0f16ba028816c6802673abfa44bd7c436
Josiah <93512794+Josiah4life@users.noreply.github.com>
2025-09-09 10:45:12 +0100

    setup MotionFolio workspace && feat(portfolio1): setup

---
+++ react_motion/src/App.jsx
@@ -0,0 +1,29 @@
+import "./app.scss"
+import "./styles.css"
+
+const App = () => {
+  return (
+    <div>
+      <section className="here">
+        Hero
+      </section>
+      <section>
+        Parallax
+      </section>
+      <section>
+        Services
+      </section>
+      <section>
+        PortFolio1
+      </section>
+      <section>
+        PortFolio2
+      </section>
+      <section>
+        PortFolio3
+      </section>
+    </div>
+  )
+};
+
+export default App;
